4bf2b1e12a651ae29530a0e01fa903cb3f1a82ba
sumonkaysar <msk.sumon.kaysar@gmail.com>
2024-06-14 08:45:37 +0600

    added users api

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,39 @@
 const { usersCollection } = require("../mongoDBConfig/collections")
 
-const getAllUsers = async (req, res) => {
-    const users = await usersCollection.find({}).toArray()
-    res.json(users)
+const saveUser = async (req, res) => {
+    const user = req.body
+    const result = await usersCollection.insertOne(user)
+    const token = jwt.sign({ user: { email: user.email } }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+    res.json({ result, token })
+}
+
+const saveProviderUser = async (req, res) => {
+    const user = req.body
+    const existedUser = await usersCollection.findOne({ email: user.email })
+    const token = jwt.sign({ user: { email: user.email } }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+    if (existedUser?.email) {
+        return res.json({ token })
+    }
+    const result = await usersCollection.insertOne(user)
+    res.json({ result, token })
+}
+
+const getOneUser = async (req, res) => {
+    const { email } = req.params
+    const user = await usersCollection.findOne({ email })
+    res.json(user)
+}
+
+const updateUser = async (req, res) => {
+    const { email } = req.params
+    const updatedInfo = req.body
+    const result = await usersCollection.updateOne({ email }, { $set: updatedInfo })
+    res.json(result)
 }
 
 module.exports = {
-    getAllUsers,
+    saveUser,
+    saveProviderUser,
+    getOneUser,
+    updateUser,
 }
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,7 +1,14 @@
-const { getAllUsers } = require("../controllers/usersController")
+const { saveUser, updateUser, getOneUser, saveProviderUser } = require("../controllers/usersController")
+const verifyUser = require("../middlewares/verifyUsers")
 
 const usersRouter = require("express").Router()
 
-usersRouter.get("/", getAllUsers)
+usersRouter.post("/", saveUser)
+
+usersRouter.post("/provider", saveProviderUser)
+
+usersRouter.get("/:email", verifyUser, getOneUser)
+
+usersRouter.patch("/:email", verifyUser, updateUser)
 
 module.exports = usersRouter
